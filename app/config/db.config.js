752597ca92550c5b23a6648a90f2cbc32fd31032
luisaarias <ariasl@utb.edu.co>
2025-04-25 17:40:54 -0500

    actualizacion la api

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  HOST: process.env.DB_HOST || "localhost",
-  USER: process.env.DB_USER || "root",
+  HOST: process.env.DB_HOST || "20.73.7.112",
+  USER: process.env.DB_USER || "dbuser",
   PASSWORD: process.env.DB_PASSWORD || "123456",
-  DB: process.env.DB_NAME || "testdb"
+  DB: process.env.DB_NAME || "testdb",
+  PORT: process.env.DB_PORT || 3306
 };
